f143f2fe84809160221e000284ece1f293a5ac4d
Nivm200 <nivm200@gmail.com>
2021-01-27 01:10:22 +0200

    Finished tooltip behaviour

--- Chapter4/tooltip-behaviour/script.js
+++ Chapter4/tooltip-behaviour/script.js
@@ -1,38 +1,24 @@
+function showTooltip(event) {
+  let tar = event.target;
+  toolTip = document.createElement("div");
+  toolTip.className = "tooltip";
+  if (tar.tagName === "BUTTON") {
+    toolTip.innerHTML = tar.dataset.tooltip;
 
-retrieveLinks();
-function retrieveLinks(){
-    let links = document.links;
-    for(let i=0; i < links.length; i++){
-     link = links[i];
-     if(link.title !== ''){
-        link.addEventListener('mouseover',createTip);
-        link.addEventListener('mouseout',cancelTip);
-     }
-    //  console.log(a);
-  } //END FOR
+    document.body.append(toolTip);
+    let xy = tar.getBoundingClientRect();
 
+    let top = xy.top - toolTip.offsetHeight - 5;
+    if (top < 0) {
+      top = xy.bottom + 5;
+    }
+    let left = xy.left + (tar.offsetWidth - toolTip.offsetWidth) / 2;
+    if (left < 0) {
+      left = 0;
+    }
+    toolTip.style.top = top + "px";
+    toolTip.style.left = left + "px";
+  }
 }
-
-// function showTip(show){
-//     if(show){
-//         var title = this.title;
-//         this.title = '';
-//         this.setAttribute("tooltip", title);
-//     }else{
-//         var title = this.getAttribute("tooltip");
-//         this.title = title;
-//         this.removeAttribute("tooltip");
-//     }
-// }
-
-function createTip(ev){
-    var title = this.title;
-    this.title = '';
-    this.setAttribute("tooltip", title);
-}
-
-function cancelTip(ev){
-var title = this.getAttribute("tooltip");
-this.title = title;
-this.removeAttribute("tooltip");
-}
+document.onmouseover = showTooltip;
+document.onmouseout = () => toolTip.remove();
