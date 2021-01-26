dad343096a6f7550dc1b5f74b37a221f3947eda2
Nivm200 <nivm200@gmail.com>
2021-01-27 01:30:17 +0200

    Finished C5Q1

--- Chapter5/selected-list/script.js
+++ Chapter5/selected-list/script.js
@@ -1,4 +1,13 @@
-let lis = document.querySelectorAll("li");
-for(li of lis){
-    li.
-}
+ul.addEventListener("click", (e) => {
+  if (!e.ctrlKey && !e.metaKey) {
+    for (let li of ul.children) {
+      li.classList.remove("selected");
+    }
+  }
+  if (e.target.tagName === "LI") {
+    e.target.classList.add("selected");
+  }
+});
+
+//Cancel double click.
+ul.onmousedown = () => false;
